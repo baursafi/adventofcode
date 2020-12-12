637fbce1b7af235c318771885179023266419b8f
Baurjan Safi <baurjan.safi@gmail.com>
2020-12-12 10:42:58 -0500

    in progress

--- 2020/Dec7/solver.py
+++ 2020/Dec7/solver.py
@@ -67,3 +67,59 @@ def bags_in_bags(initial_color):
 
 number_of_bags = bags_in_bags('shiny gold')
 
+"""
+--- Part Two ---
+It's getting pretty expensive to fly these days - not because of ticket prices, 
+but because of the ridiculous number of bags you need to buy!
+
+Consider again your shiny gold bag and the rules from the above example:
+
+faded blue bags contain 0 other bags.
+dotted black bags contain 0 other bags.
+vibrant plum bags contain 11 other bags: 5 faded blue bags and 6 dotted black bags.
+dark olive bags contain 7 other bags: 3 faded blue bags and 4 dotted black bags.
+So, a single shiny gold bag must contain 1 dark olive bag (and the 7 bags within it) plus 
+2 vibrant plum bags (and the 11 bags within each of those): 1 + 1*7 + 2 + 2*11 = 32 bags!
+
+Of course, the actual rules have a small chance of going several levels deeper than this example; 
+be sure to count all of the bags, even if the nesting becomes topologically impractical!
+
+Here's another example:
+
+shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.
+In this example, a single shiny gold bag must contain 126 other bags.
+
+How many individual bags are required inside your single shiny gold bag?
+"""
+import re
+
+def bag_counter(color):
+    counter = 0
+    contains = [line.split('bags contain')[1].strip().replace('.','') for line in inp if color in line.split('bags contain')[0].strip() and 'no other' not in line.split('bags contain')[0].strip()][0]  
+    if 'no other bags' in contains:
+        return None, None 
+    contains = contains.split(',')
+    counter = [eval(bags.strip()[0]) for bags in contains]
+    bags_types = [' '.join(bags.strip().split()[1:-1]) for bags in contains]
+    return counter, bags_types
+
+def rec_bc(initial_color, inp = inp):
+    counter = []
+    bags = []
+    if 
+    counter += bag_counter(initial_color)[0]
+    bags += bag_counter(initial_color)[1]
+    for bag in bag_counter(initial_color)[1]:
+        print(bag)
+        counter += bag_counter(bag)[0]
+        bags += bag_counter(bag)[1]
+    return counter, bags 
+
+bag_counter('shiny gold')
+rec_bc('shiny gold')
