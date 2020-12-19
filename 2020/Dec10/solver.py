8822adf7b7d8b8af5a64ff09638fb3701031e2e7
Baurjan Safi <baurjan.safi@gmail.com>
2020-12-19 18:12:01 -0500

    Added day 10

--- 2020/Dec10/solver.py
+++ 2020/Dec10/solver.py
@@ -118,3 +118,126 @@ while i<len(inpt)-1:
 cd = Counter(differences)
 output = cd[1] * cd[3]
 print(output)
+
+"""
+--- Part Two ---
+To completely determine whether you have enough adapters, 
+you'll need to figure out how many different ways they can be arranged. 
+Every arrangement needs to connect the charging outlet to your device. 
+The previous rules about when adapters can successfully connect still apply.
+
+The first example above (the one that starts with 16, 10, 15) supports 
+the following arrangements:
+
+(0), 1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, (22)
+(0), 1, 4, 5, 6, 7, 10, 12, 15, 16, 19, (22)
+(0), 1, 4, 5, 7, 10, 11, 12, 15, 16, 19, (22)
+(0), 1, 4, 5, 7, 10, 12, 15, 16, 19, (22)
+(0), 1, 4, 6, 7, 10, 11, 12, 15, 16, 19, (22)
+(0), 1, 4, 6, 7, 10, 12, 15, 16, 19, (22)
+(0), 1, 4, 7, 10, 11, 12, 15, 16, 19, (22)
+(0), 1, 4, 7, 10, 12, 15, 16, 19, (22)
+(The charging outlet and your device's built-in adapter are shown in parentheses.) 
+Given the adapters from the first example, the total number of arrangements 
+that connect the charging outlet to your device is 8.
+
+The second example above (the one that starts with 28, 33, 18) has 
+many arrangements. Here are a few:
+
+(0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+32, 33, 34, 35, 38, 39, 42, 45, 46, 47, 48, 49, (52)
+
+(0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+32, 33, 34, 35, 38, 39, 42, 45, 46, 47, 49, (52)
+
+(0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+32, 33, 34, 35, 38, 39, 42, 45, 46, 48, 49, (52)
+
+(0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+32, 33, 34, 35, 38, 39, 42, 45, 46, 49, (52)
+
+(0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+32, 33, 34, 35, 38, 39, 42, 45, 47, 48, 49, (52)
+
+(0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+46, 48, 49, (52)
+
+(0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+46, 49, (52)
+
+(0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+47, 48, 49, (52)
+
+(0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+47, 49, (52)
+
+(0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+48, 49, (52)
+In total, this set of adapters can connect the charging outlet to your 
+device in 19208 distinct arrangements.
+
+You glance back down at your bag and try to remember why you brought 
+so many adapters; there must be more than a trillion valid ways to arrange them! Surely, there must be an efficient way to count the arrangements.
+
+What is the total number of distinct ways you can arrange the adapters to connect the charging outlet to your device?
+"""
+
+# we can only mix and match adapters that are 1 jolt from each other
+# this way, we need to find, number of combinations that we can
+# get from clusters made up of 1,2, or 3 adapters
+# typically we could use 
+# sum(n! / (r!(n-r)!) for r between )
+# so first, let's define factorial function"
+
+def factorial(n):
+    if n==0:
+        return 1 
+    else:
+        return n * factorial(n-1)
+
+# let's test it
+factorial(4)
+factorial(6)
+# in a cluster of 3 adapters we can only remove 0 to 2 at a time
+# so for such clusters the number of combinations would be
+
+def cluster_combinations(cluster_length, max_steps):
+    if cluster_length == 1:
+        return 2
+    combinations = 0
+    for i in range(0,max_steps,1):
+        combinations += factorial(cluster_length)\
+            /(factorial(cluster_length - i)\
+                *factorial(i))
+    return combinations
+
+
+cluster_combinations(2, 3)
+
+# now let's register the clusters or removeable adapters
+# for simplicity we'll use "differences" instead of the 
+# original input "inpt" list
+
+clusters = []
+cluster_counter = 0
+for index, jolt_step in enumerate(differences[:-1]):
+    if jolt_step == 1 and differences[index + 1] == 1:
+        print(jolt_step, differences[index+1])
+        cluster_counter += 1
+    elif cluster_counter > 0:
+        clusters.append(cluster_counter)
+        cluster_counter = 0
+
+# now that we have found the clusters, let's get the number 
+# combinations
+combinations = [cluster_combinations(cluster_length = i, max_steps = 3) for i in clusters]
+
+# now we need to multiply all elements in the list of 
+# combinations to get the overall number of combinations
+result = 1
+for i in combinations:
+    result *= i
+
+# The asnwer is 28,346,956,187,648
+result
+
